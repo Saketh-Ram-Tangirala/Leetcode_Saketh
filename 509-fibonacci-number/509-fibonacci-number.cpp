faebefecff6afc336d956a101e95711ae1cd7d85
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-05-25 00:57:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (10.08%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
         unordered_map<int,int> mp;
-        
     int fib(int n) {
         if(n==0 || n==1)
         {
                 return n;
         }
             int currentKey=n;
-         if(mp.find(currentKey)!=mp.end())
-         {
-               return   mp[currentKey];
-         }
+            if(mp.find(currentKey)!=mp.end())
+            {
+                    return mp[currentKey];
+            }
             mp[n]=fib(n-1)+fib(n-2);
             return mp[currentKey];
     }
