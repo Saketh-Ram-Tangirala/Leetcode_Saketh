ccc2b8340f6dd310d7e38cb580e980df406e214f
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-05-25 01:03:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.98%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int tribonacci(int n) {
         unordered_map<int,int> mp;
-        return nthTrib(n,mp);
-    }
-    int nthTrib(int n,unordered_map<int,int> &mp)
-    {
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1 || n==2)
-        {
-            return 1;
-        }
-        int currentKey=n;
-        if(mp.find(currentKey)!=mp.end())
-        {
+    int tribonacci(int n) {
+            if(n==0 || n==1)
+            {
+                    return n;
+            }
+            if(n==2)
+            {
+                    return 1;
+            }
+            int currentKey=n;
+            if(mp.find(currentKey)!=mp.end())
+            {
+                    return mp[currentKey];
+            }
+            mp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
             return mp[currentKey];
-        }
-        
-         int a=nthTrib(n-1,mp);
-        int b=nthTrib(n-2,mp);
-        int c=nthTrib(n-3,mp);
-        mp[currentKey]=a+b+c;
-        return mp[currentKey];
         
     }
 };
