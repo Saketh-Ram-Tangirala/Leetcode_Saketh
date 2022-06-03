fedc56c71a488aa3da1db8d0577e5f267f36fda6
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-06-03 10:43:54 +0530

    Time: 13 ms (55.77%), Space: 7.4 MB (46.68%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   
+    bool isAnagram(string s1, string s2) {
+             const int CHAR=256;
+        if(s1.length()!=s2.length())
+        {
+                return false;
+        }
+            int count[CHAR]={0};
+         for(int i=0;i<s1.length();i++)
+         {
+                 count[s1[i]]++;
+                 count[s2[i]]--;
+         }
+            for(int i=0;i<CHAR;i++)
+            {
+                    if(count[i]!=0)
+                    {
+                            return false;
+                    }
+            }
+            return true;
+    }
+};
