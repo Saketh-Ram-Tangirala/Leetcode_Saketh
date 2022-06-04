0283dceb279c24533c71b3fec96edc9c1529a7ac
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-06-04 23:53:02 +0530

    Time: 8 ms (71.11%), Space: 6 MB (78.81%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-          vector<pair<int, string>> map{{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"},{50, "L"}, {90, "XC"}, {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"}};
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
-        int i = map.size()-1;
-        string res = "";
-        while(num!=0){
-            while(map[i].first > num){
-                i--;
+        string ans="";
+        int i=0;
+        while(num){
+            while(val[i]<=num){
+                num -= val[i];
+                ans += sym[i];
             }
-            res = res + map[i].second;
-            num = num - map[i].first;
+            i++;
         }
-        return res;
+        
+        return ans;
     }
 };
