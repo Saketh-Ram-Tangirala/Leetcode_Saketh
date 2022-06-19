a169a98bc24b095bf3150a4f75906f5db9e0c5ad
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-06-20 01:53:54 +0530

    Time: 3 ms (70.83%), Space: 10.7 MB (37.36%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -6,20 +6,17 @@ public:
             generateSubsets(nums,0,vec,ans);
             return ans;
     }
-    void generateSubsets(vector<int>&nums,int ci,vector<int>&vec,vector<vector<int>>&ans)
-    {
-            if(ci>=nums.size())
-            {
-                    ans.push_back(vec);
-                    return;
-            }
-            int currval=nums[ci];
-            vec.push_back(currval);
-            generateSubsets(nums,ci+1,vec,ans);
-            vec.pop_back();
-            generateSubsets(nums,ci+1,vec,ans);
-            
-            
-            
-    }
+     void generateSubsets(vector<int>&nums,int ci,vector<int>&vec,vector<vector<int>>&ans)
+     {
+             if(ci>nums.size()-1)
+             {
+                     ans.push_back(vec);
+                     return;
+             }
+             int currval=nums[ci];
+             vec.push_back(currval);
+             generateSubsets(nums,ci+1,vec,ans);
+             vec.pop_back();
+             generateSubsets(nums,ci+1,vec,ans);
+     }
 };
