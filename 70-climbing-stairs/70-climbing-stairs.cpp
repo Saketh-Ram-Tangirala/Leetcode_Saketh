ecb4736c9c86b858402cb0fcbee0ba4b349c654c
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-06-29 23:41:32 +0530

    Time: 5 ms (11.34%), Space: 6.3 MB (14.77%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    
+        int totalways(int curr,int target,unordered_map<int,int>&mp)
+    {
+        if(curr==target)return 1;
+        if(curr>target) return 0;
+        int currkey = curr;
+        if(mp.find(currkey)!=mp.end())
+            return mp[curr];
+        int onestep = totalways(curr+1,target,mp);
+        int twostep = totalways(curr+2,target,mp);
+        mp[curr]=onestep+twostep;
+        return onestep+twostep;
+    }
     int climbStairs(int n) {
-       if(n==1 || n==2)return n;
-       int a=1,b=2,sum=0;
-       for(int i=3;i<=n;i++)
-       {
-           sum=(a+b);
-           a=b;
-           b=sum;
-       }
-       return sum;   
+        unordered_map<int,int>mp;
+        return totalways(0,n,mp);
+         
     }
 };
