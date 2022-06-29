703191181b455ad9dc8b086ccc6d4b7273b40385
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-06-30 00:32:57 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (5.94%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
         unordered_map<int,int> mp;
-            return amount(0,nums,mp);
+    int rob(vector<int>& nums) {
+        return houserob(0,nums);
+            
     }
-        int amount(int ci,vector<int>&nums,unordered_map<int,int>&mp)
-        {
-                if(ci>=nums.size())
-                {
-                        return 0;
-                }
-                 int currentKey=ci;
-                 if(mp.find(currentKey)!=mp.end())
-                {
-                        return mp[currentKey];
-                }
-                int yesrob=nums[ci]+amount(ci+2,nums,mp);
-                int norob=amount(ci+1,nums,mp);
-               
-               
-                mp[currentKey]=max(yesrob,norob);
-                return mp[currentKey];
-        }
+     int houserob(int ci,vector<int>&nums)
+     {
+             if(ci>=nums.size())
+             {
+                     return 0;
+             }
+             int currentKey=ci;;
+             if(mp.find(currentKey)!=mp.end())
+             {
+                     return mp[ci];
+             }
+             int yesrob=nums[ci]+houserob(ci+2,nums);
+             int norob=houserob(ci+1,nums);
+             mp[currentKey]=max(yesrob,norob);
+             return mp[ci];
+             
+     }
 };
