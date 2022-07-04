e4ca9aea5f4ca79bc91d86ea63173fd465db7c8d
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-04 18:11:34 +0530

    Time: 20 ms (42.59%), Space: 17.1 MB (40.24%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-       vector<vector<int>> ans;
-            vector<int> vec;
-            CombinationalSum(candidates,0,target,vec,ans);
+        vector<vector<int>> ans;
+            vector<int> v;
+            CombinationalSum(candidates,0,target,v,ans);
             return ans;
     }
-     void CombinationalSum(vector<int>&candidates,int ci,int target,vector<int>&vec,vector<vector<int>>&ans)
-     {
-             if(target==0)
-             {
-                     ans.push_back(vec);
-                     return;
-             }
-             if(ci>=candidates.size())
-             {
-                     return;
-             }
-             int currval=candidates[ci];
-             if(currval<=target)
-             {
-                     vec.push_back(currval);
-                     CombinationalSum(candidates,ci,target-currval,vec,ans);
-                     vec.pop_back();
-             }
-             CombinationalSum(candidates,ci+1,target,vec,ans);
-     }
+        void CombinationalSum(vector<int>&candidates,int ci,int target,vector<int>&v,vector<vector<int>>&ans)
+        {
+                if(target==0)
+                {
+                       
+                        ans.push_back(v);
+                        return;
+                }
+                if(ci>=candidates.size())
+                {
+                        return;
+                }
+                int currval=candidates[ci];
+                if(target>=currval)
+                {
+                        v.push_back(currval);
+                        CombinationalSum(candidates,ci,target-candidates[ci],v,ans);
+                        v.pop_back();
+                }
+                CombinationalSum(candidates,ci+1,target,v,ans);
+        }
+        
 };
