8d7543f9af5208d94d39323614f3c428b6d25f90
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-04 15:48:58 +0530

    Time: 20 ms (64.33%), Space: 10.7 MB (69.18%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-         int n=graph.size();
-          vector<bool> visited(n);
-            vector<int> path;
-            SourceToTarget(graph,0,visited,n,path,ans);
+       vector<vector<int>> ans;
+            vector<int> v;
+             int n=graph.size();
+            vector<bool> visited(n);
+           
+            SourceToTarget(graph,0,n,v,visited,ans);
             return ans;
     }
-        void SourceToTarget(vector<vector<int>>& graph,int ci,vector<bool>&visited,int n,vector<int>&path,vector<vector<int>>&ans)
+        void SourceToTarget(vector<vector<int>>&graph,int ci,int n,vector<int>&v,vector<bool>&visited,vector<vector<int>>&ans)
         {
                 if(ci==n-1)
                 {
-                        path.push_back(ci);
-                        ans.push_back(path);
-                        path.pop_back();
+                        v.push_back(ci);
+                        ans.push_back(v);
+                        v.pop_back();
                         return;
                 }
                 if(visited[ci]==true)
@@ -22,13 +23,13 @@ public:
                         return;
                 }
                 visited[ci]=true;
-                path.push_back(ci);
-                 for(auto neighbour: graph[ci])
-            SourceToTarget(graph,neighbour,visited,n,path,ans);
-        
-        visited[ci] = false;
-        path.pop_back();
-        return;
+                v.push_back(ci);
+                for(auto neighbour:graph[ci])
+                {
+                        SourceToTarget(graph,neighbour,n,v,visited,ans);
+                }
+                visited[ci]=false;
+                v.pop_back();
+                return;
         }
-                
 };
