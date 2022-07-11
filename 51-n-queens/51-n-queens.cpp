afdfcd0ab9f903f08786ad9de907d230ad2b7bff
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-11 13:27:08 +0530

    Time: 22 ms (27.59%), Space: 8.1 MB (31.33%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,56 +1,85 @@
 class Solution {
 public:
-    bool isValid(int curr_row,int curr_col,vector<vector<char>>&grid,int n)
-    {
-        return isRowValid(curr_row,grid,n) && isColValid(curr_col,grid,n) && areDiagValid(curr_row,curr_col,grid,n);
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+         vector<vector<char>> grid(n,vector<char> (n,'.'));
+            solve(grid,0,n,ans);
+            return ans;
     }
-    bool isRowValid(int curr_row,vector<vector<char>>&grid,int n)
-    {
-        for(int j=0;j<n;j++)
+        void solve(vector<vector<char>>&grid,int cr,int n,vector<vector<string>>&ans)
         {
-            if(grid[curr_row][j]=='Q')
-                return false;
+                if(cr==n)
+                {
+                        vector<string> tempAns=populate(grid,n);
+                        ans.push_back(tempAns);
+                        return ;
+                }
+                for(int cc=0;cc<n;cc++)
+                {
+                        if(isValid(grid,cr,cc,n))
+                        {
+                                grid[cr][cc]='Q';
+                                solve(grid,cr+1,n,ans);
+                                grid[cr][cc]='.';
+                        }
+                        
+                }
+                return;
         }
-        return true;
-    }
-    bool isColValid(int curr_col,vector<vector<char>>&grid,int n)
-    {
-        for(int j=0;j<n;j++)
+        bool isValid(vector<vector<char>>&grid,int cr,int cc,int n)
         {
-            if(grid[j][curr_col]=='Q')
-                return false;
+                return isrow(grid,cr,n) && iscol(grid,cc,n) && arediag(grid,cr,cc,n);
         }
-        return true;
-     
-    }
-    bool areDiagValid(int curr_row,int curr_col,vector<vector<char>>&grid,int n)
+        bool isrow(vector<vector<char>>&grid,int cr,int n)
+        {
+                for(int j=0;j<n;j++)
+                {
+                        if(grid[cr][j]=='Q')
+                        {
+                                return false;
+                        }
+                }
+                return true;
+        }
+        bool iscol(vector<vector<char>>&grid,int cc,int n)
+        {
+                for(int j=0;j<n;j++)
+                {
+                        if(grid[j][cc]=='Q')
+                        {
+                                return false;
+                        }
+                }
+                return true;
+        }
+      bool arediag(vector<vector<char>>&grid,int cr,int cc,int n)
     {
-        int i = curr_row;
-        int j = curr_col;
+        int i = cr;
+        int j = cc;
         while(i>=0 && j>=0) // first diag
         {
             if(grid[i][j]=='Q') return false;
             i--;
             j--;
         }
-         i = curr_row;
-         j = curr_col;
+         i = cr;
+         j = cc;
         while(i>=0 && j<n) // second diag
         {
             if(grid[i][j]=='Q') return false;
             i--;
             j++;
         }
-         i = curr_row;
-         j = curr_col;
+         i = cr;
+         j = cc;
         while(i<n && j>=0) // third diag
         {
             if(grid[i][j]=='Q') return false;
             i++;
             j--;
         }
-         i = curr_row;
-         j = curr_col;
+         i = cr;
+         j = cc;
         while(i<n && j<n) // fourth diag
         {
             if(grid[i][j]=='Q') return false;
@@ -59,43 +88,20 @@ public:
         }
         return true;
     }
-    vector<string>populate(vector<vector<char>>&grid,int n)
-    {
-        vector<string>vec;
-        for(int currrow=0;currrow<n;currrow++)
+        vector<string> populate(vector<vector<char>>&grid,int n)
         {
-            string temp = "";
-            for(int currcol=0;currcol<n;currcol++)
-            {
-                temp+=grid[currrow][currcol];
-            }
-            vec.push_back(temp);
-        }
-        return vec;
-    }
-    void solve(vector<vector<char>>&grid,int curr_row,int n , vector<vector<string>>&ans )
-    {
-        if(curr_row==n)
-        {
-            vector<string>tempans = populate(grid,n);
-            ans.push_back(tempans);
-            return;
-        }
-            for(int curr_col = 0;curr_col<n;curr_col++)
-            {
-                if(isValid(curr_row,curr_col,grid,n))
+                vector<string> vec;
+                for(int cr=0;cr<n;cr++)
                 {
-                    grid[curr_row][curr_col]='Q';
-                    solve(grid,curr_row+1,n,ans);
-                    grid[curr_row][curr_col]='.'; // undoing the change and replacing it with dot
+                        string temp="";
+                        for(int cc=0;cc<n;cc++)
+                        {
+                                temp+=grid[cr][cc];
+                                
+                        }
+                        vec.push_back(temp);
+                        
                 }
-            }
-        return;
-    }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<vector<char>>grid(n,vector<char>(n,'.'));
-        solve(grid,0,n,ans);
-        return ans;
-    }
+                return vec;
+        }
 };
