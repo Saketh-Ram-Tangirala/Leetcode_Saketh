25e4a223da4a668f935d9953d6b3e400431d7fbd
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-17 05:19:10 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,15 +4,16 @@ public:
             if(n==0 || n==1)
             {
                     return n;
-            }
-            vector<int> dp(n+1,0);
-               dp[0]=0;
-               dp[1]=1;
-            for(int i=2;i<=n;i++)
-            {
-                    dp[i]=dp[i-1]+dp[i-2];
                     
             }
-            return dp[n];
+           int prev=1;
+            int prev2=0;
+            for(int i=2;i<=n;i++)
+            {
+                    int curr=prev+prev2;
+                    prev2=prev;
+                    prev=curr;
+            }
+            return prev;
     }
 };
