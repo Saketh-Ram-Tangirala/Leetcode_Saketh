40a065bd3e83331b4f4d3baf5c0c8a5166b2976f
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-17 05:29:00 +0530

    Time: 4 ms (22.52%), Space: 6.4 MB (15.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int climbStairs(int n) {
-           vector<int> dp(n+1,-1);
-  
-        int prev2= 1;
-         int prev1= 1;
-            int curr;
-  
-  for(int i=2; i<=n; i++){
-           curr= prev1+ prev2;
-          prev2=prev1;
-          prev1=curr;
-  }
-         return prev1;
+            unordered_map<int,int> mp;
+      return nthStair(0,n,mp);   
     }
+        int nthStair(int cs,int ts,unordered_map<int,int>&mp)
+        {
+                if(cs==ts)
+                {
+                        return 1;
+                }
+                if(cs>ts)
+                {
+                        return 0;
+                }
+                int currentKey=cs;
+                if(mp.find(currentKey)!=mp.end())
+                {
+                        return mp[currentKey];
+                }
+                int a=nthStair(cs+1,ts,mp);
+                int b=nthStair(cs+2,ts,mp);
+                mp[cs]=a+b;
+                return mp[cs];
+        }
 };
