df972fa7bef7ce537610dcccf5465220012cd337
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-17 14:51:10 +0530

    Time: 1524 ms (5.00%), Space: 268.1 MB (5.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-             vector<vector<vector<int>>> vec(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
-          return besttime(prices,0,true,1,vec);
-            
+    int bestTrans(vector<int>&prices,int curr,bool canbuy , int trans,vector<vector<vector<int>>>&vec)
+    {
+        
+        if(curr>=prices.size()) return 0;
+        if(trans<=0) return 0;
+        if(vec[curr][canbuy][trans]!= -1)
+           return vec[curr][canbuy][trans];
+        if(canbuy)
+        {
+            int idle = bestTrans(prices,curr+1,canbuy,trans,vec); 
+            int buy = -prices[curr]+bestTrans(prices,curr+1,!canbuy,trans,vec);
+            return vec[curr][canbuy][trans]=max(idle,buy);
+        }
+        else{
+            int idle1 = bestTrans(prices,curr+1,canbuy,trans,vec);
+            int sell = prices[curr]+bestTrans(prices,curr+1,canbuy,trans-1,vec);
+            return vec[curr][canbuy][trans]=max(idle1,sell);
+        }
+        
     }
-     int besttime(vector<int>&prices,int currentDay,bool canBuy,int trans,vector<vector<vector<int>>>&vec)
-     {
-             if(currentDay>=prices.size() || trans<=0)
-             {
-                     return 0;
-             }
-              if(vec[currentDay][canBuy][trans]!= -1)
-           return vec[currentDay][canBuy][trans];
-             if(canBuy)
-             {
-                     int idle=besttime(prices,currentDay+1,canBuy,trans,vec);
-                     int buy=-prices[currentDay]+besttime(prices,currentDay+1,false,trans,vec);
-                      return vec[currentDay][canBuy][trans]=max(idle,buy);
-             }
-             else
-             {
-                       int idle=besttime(prices,currentDay+1,canBuy,trans,vec);
-                       int sell=prices[currentDay]+besttime(prices,currentDay+1,true,trans-1,vec);
-                      return vec[currentDay][canBuy][trans]=max(idle,sell);
-             }
-             
-     }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>> vec(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+        return bestTrans(prices,0,1,1,vec);
+        
+    }
 };
