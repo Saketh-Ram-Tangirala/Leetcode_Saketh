d4f0f4e8e937cc588a3941611c2fd2285d18d589
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-26 12:00:34 +0530

    Time: 24 ms (5.92%), Space: 15.5 MB (5.04%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    int mincost(vector<int>&cost , int curr)
-    {
-        if (curr == cost.size())
-            return 0;
-        if (curr > cost.size())
-            return 1000;
-        if(mp.find(curr)!=mp.end())
-            return mp[curr];
-        int onejump = cost[curr]+mincost(cost,curr+1);
-        int twojump = cost[curr]+mincost(cost,curr+2);
-        int currkey = curr;
-        mp[currkey] = min(onejump,twojump);
-        return min(onejump,twojump);
-        
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        int a = mincost(cost,0);
-        int b = mincost(cost,1);
-        return min(a,b);
-        
+        unordered_map<int,int> mp;
+        return min(mincost(0,cost,mp),mincost(1,cost,mp));
+    }
+    int mincost(int ci,vector<int>&cost,unordered_map<int,int>&mp)
+    {
+        if(ci>=cost.size())
+        {
+            return 0;
+        }
+        int currentKey=ci;
+        if(mp.find(currentKey)!=mp.end())
+        {
+            return mp[currentKey];
+        }
+        int onestep=cost[ci]+mincost(ci+1,cost,mp);
+         int twostep=cost[ci]+mincost(ci+2,cost,mp);
+       mp[currentKey]=min(onestep,twostep);
+        return min(onestep,twostep);
     }
 };
