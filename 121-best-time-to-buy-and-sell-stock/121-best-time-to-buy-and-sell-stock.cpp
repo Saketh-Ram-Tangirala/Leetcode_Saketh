00a3fc722ce80a3d213d5ef4e0b27848b579deae
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-07-26 11:50:36 +0530

    Time: 853 ms (5.00%), Space: 268 MB (5.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    int maxProfit(vector<int>& prices) {
+         vector<vector<vector<int>>> vec(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+          return bestTrans(prices,0,1,1,vec);
+    }
     int bestTrans(vector<int>&prices,int curr,bool canbuy , int trans,vector<vector<vector<int>>>&vec)
     {
-        
-        if(curr>=prices.size()) return 0;
+              if(curr>=prices.size()) return 0;
         if(trans<=0) return 0;
         if(vec[curr][canbuy][trans]!= -1)
            return vec[curr][canbuy][trans];
@@ -18,11 +21,5 @@ public:
             int sell = prices[curr]+bestTrans(prices,curr+1,canbuy,trans-1,vec);
             return vec[curr][canbuy][trans]=max(idle1,sell);
         }
-        
-    }
-    int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> vec(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
-        return bestTrans(prices,0,1,1,vec);
-        
     }
 };
