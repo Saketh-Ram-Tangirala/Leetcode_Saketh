45073e038a667a38ac59318fbba289ee90aef68f
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-08-15 16:26:21 +0530

    Time: 342 ms (38.30%), Space: 18.4 MB (46.37%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,9 +4,4 @@ class Solution:
         Do not return anything, modify s in-place instead.
         
         """
-        start=0
-        end=len(s)-1
-        while(start<end):
-            s[start],s[end]=s[end],s[start]
-            start=start+1
-            end=end-1
+        s.reverse()
