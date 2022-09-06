0c76a22953bd92a700637b286a276dae3ac84a72
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-09-06 21:49:56 +0530

    Time: 30 ms (6.04%), Space: 15.6 MB (17.92%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
        vector<pair<char,int>> v(indices.size());
-    
-    for(int i=0;i<indices.size();i++)
-    {
-        v[i].first=indices[i];
-        v[i].second=s[i];
-    }
-    
-    sort(v.begin(),v.end());
-    
-    string s1="";
-    
-    for(int i=0;i<s.size();i++)
-    {
-        s1+=v[i].second;
-    }
-    return s1;
+        int n=indices.size();
+        for(int i=0;i<n;i++)
+        {
+            v[i].first=indices[i];
+            v[i].second=s[i];
+        }
+        sort(v.begin(),v.end());
+        string str;
+        for(int i=0;i<v.size();i++)
+        {
+            str+=v[i].second;
+        }
+        return str;
     }
 };
