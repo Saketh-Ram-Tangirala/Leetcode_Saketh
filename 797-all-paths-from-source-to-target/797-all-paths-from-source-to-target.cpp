3978667418dc33b903d65e38123917e80cc23088
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-10-02 14:49:31 +0530

    Time: 15 ms (84.28%), Space: 10.6 MB (81.98%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> answer;
         int n=graph.size();
-        vector<bool> vis(n);
-        
         vector<int> vec;
-        
+        vector<vector<int>> answer;
+        vector<bool> vis(n);
         vec.push_back(0);
         findAllpaths(graph,0,vis,n,vec,answer);
-        
         return answer;
-        
-        }
+    }
     void findAllpaths(vector<vector<int>>& graph,int curr,vector<bool>&vis,int n,vector<int>&vec,vector<vector<int>>&answer)
     {
         if(curr==n-1)
@@ -25,14 +21,12 @@ public:
             return;
         }
         vis[curr]=true;
-        for(auto neighbour:graph[curr])
+        for(auto neigh:graph[curr])
         {
-            vec.push_back(neighbour);
-            findAllpaths(graph,neighbour,vis,n,vec,answer);
+            vec.push_back(neigh);
+            findAllpaths(graph,neigh,vis,n,vec,answer);
             vec.pop_back();
-            
         }
         vis[curr]=false;
-        return;
     }
 };
