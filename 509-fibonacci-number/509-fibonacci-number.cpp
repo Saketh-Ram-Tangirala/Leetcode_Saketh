8682dc9413cfc433f128050fd89cd6c9d9d8d578
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-10-02 15:42:39 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-      vector<int> dp(n+1);
-        if(n == 0)
-            return 0;
-        
-        dp[0] = 0;
-        dp[1] = 1;
-        
-       
-        //start from 2
-        
-        for(int i = 2; i<=n; i++)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        
-        return dp[n];
+       if(n == 0 )return 0; 
+		int prev=1, prev1=0; 
+		int curr=0 ; 
+		for(int i=2;i<=n;i++){
+			curr = prev+prev1;
+			prev1= prev; 
+			prev = curr; 
+		}
+		return prev; 
     }
-    
 };
