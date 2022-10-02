d26f97c209d30d8e4eba2e288559983df83dcfe5
Saketh-Ram-Tangirala <73960795+Saketh-Ram-Tangirala@users.noreply.github.com>
2022-10-02 15:37:43 +0530

    Time: 3 ms (64.66%), Space: 6 MB (16.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return nthFibb(n,dp);
-    }
-    int nthFibb(int n,vector<int>&dp)
-    {
-        if(n<=1)
+      vector<int> dp(n+1);
+        if(n == 0)
+            return 0;
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+       
+        //start from 2
+        
+        for(int i = 2; i<=n; i++)
         {
-            return n;
-            
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-        return dp[n]=nthFibb(n-1,dp)+nthFibb(n-2,dp);
+        
+        return dp[n];
     }
+    
 };
